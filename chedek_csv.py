d1b66fd697945a2fd5b974ec17489c1723089784
Asaf David <asaf.david@allcloud.io>
2019-11-07 15:33:57 +0200

    finale tune

--- chedek_csv.py
+++ chedek_csv.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import os
 
+dir_to_scan = "C:\\Users\\Assaf David\\Desktop\\chedek\\"	# set variable for the dest file.
+
 def csv(old_file, new_file, columns):
     with open(old_file, 'r+', encoding = "ISO-8859-1") as f:
         with open(new_file, 'w+', encoding = "ISO-8859-1") as f_new:
@@ -21,10 +23,24 @@ def csv(old_file, new_file, columns):
                     print(line[1])
                     print(line[2])
 
-                    # f_new.seek(0, 0)								# go to the begining of the file.
-                    # f_new.write(columns + '\n')
-                    # f_new.write(line[0] + ',' + line[1] + ',' + line[2])
+                    f_new.seek(0, 0)								# go to the begining of the file.
+                    f_new.write(columns + '\n')
+                    f_new.write(line[0] + ',' + line[1] + ',' + line[2])
         f_new.close()
     f.close()
 
-csv('DemogExample.txt', 'DemogExample.csv', 'name, surename,phone')
+def check_csv_is_exist(file):								# def a function to check csv is exist
+	filename = os.fsdecode(file)							# set variable
+	if filename.endswith(".csv"):							# check if the file is a txt
+		return True											# return the file
+
+for file in os.listdir(dir_to_scan):
+    txt = check_csv_is_exist(file)
+
+    if txt:
+        columns_head = input('Please enter columns names with comma delimited: \n')		# columns name.`
+        file_new = os.fsdecode(file).replace('.txt', '.csv')
+        csv(file, file_new, columns_head)
+        print("%s is updated." % file) 													# print message
+    else:
+        print("There is no any TXT files in dest folder.")
