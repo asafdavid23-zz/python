544f413e6f4cc36d1df431795e375546d6c79529
Asaf David <asaf.david@allcloud.io>
2020-10-14 13:51:31 +0300

    Initial commit

---
+++ Hacking_The_Fender/script.py
@@ -0,0 +1,43 @@
+import csv, json
+
+compromised_users = []
+
+with open('passwords.csv') as f:
+  password_csv = csv.DictReader(f)
+
+  for row in password_csv:
+    password_row = row
+    compromised_users.append(password_row['Username'])
+
+with open('compromised.txt', 'w') as compromised_user_file:
+  for u in compromised_users:
+    compromised_user_file.write(u + "\n")
+
+with open('boss_message.json', 'w') as boss_message:
+  boss_message_dict = {
+    "racipient":"The Boss",
+    "message": "Mission Success"
+  }
+  json.dump(boss_message_dict, boss_message)
+
+with open('new_passwords.csv', 'w') as new_passwords_obj:
+  slash_null_sig = """
+    _  _     ___   __  ____             
+  / )( \   / __) /  \(_  _)            
+  ) \/ (  ( (_ \(  O ) )(              
+  \____/   \___/ \__/ (__)             
+  _  _   __    ___  __ _  ____  ____  
+  / )( \ / _\  / __)(  / )(  __)(    \ 
+  ) __ (/    \( (__  )  (  ) _)  ) D ( 
+  \_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+          ____  __     __   ____  _  _ 
+  ___   / ___)(  )   / _\ / ___)/ )( \
+  (___)  \___ \/ (_/\/    \\___ \) __ (
+        (____/\____/\_/\_/(____/\_)(_/
+  __ _  _  _  __    __                
+  (  ( \/ )( \(  )  (  )               
+  /    /) \/ (/ (_/\/ (_/\             
+  \_)__)\____/\____/\____/
+  """
+  new_passwords_obj.write(slash_null_sig)
+
